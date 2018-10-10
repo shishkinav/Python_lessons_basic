0f24aed4d284e9ea2f7f810d5cd3efcc1419a460
AnatolyShishkin <shishkinanatoli@gmail.com>
2018-10-10 20:38:00 +0300

    Урок 4 - Домашнее задание - Уровень Normal - Задача 2 - добавил код

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -30,7 +30,10 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
-
+'''
+использовал для проверок урывками
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHB'
+'''
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
        'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
@@ -46,6 +49,62 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+import re
+
+listUpperSimbol = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2) # регулярка - это зверь
+print(listUpperSimbol, 'длина списка с помощью регулярки - ', len(listUpperSimbol))
+
+# теперь выполняем задачу муторно без регулярки
+# решение мне подсказали, самостоятельно не разобрался (((, пришлось разбираться по чужому коду
+
+countUpper = 0      # вводим переменную для подсчёта количества верхнерегистровых символов
+listBig = []        # объявляем пустой список для хранения элементов
+
+# запускаем перебор индексов исходной строки начиная с третьего символа
+for indexElement in range(2, len(line_2)):
+    # если символ строки с текущим индексом верхнеристровый и при этом если есть срез двух нижнерегистровых элементов подряд
+    # срез line_2 [текущий индекс элемента - количество ранее найденных до этого верхнерегистровых элемента - 2 :
+    # текущий индекс элемента - кол-во ранее найденных до этого верхнерегистровых элемента]. кол-во верхнерегистровых
+    # используется для того, чтобы смешать срез влево на кол-во уже пройденных верхних символов и заодно считать их
+    if line_2[indexElement].isupper() and line_2[indexElement - countUpper - 2 : indexElement - countUpper].islower():
+        countUpper += 1
+    # если мы дошли до этого условия, значит очередной элемент не является верхнерегистровым, проверяем набрано ли нужно количество верхнерегистровых
+    elif countUpper <= 2:   # в данном случае не набрали нужного количества
+        countUpper = 0      # обнуляем для следующей проверки
+    elif countUpper > 2:    # а здесь набрали и передаём наш срез верхнерегистровых в список уменьшая срез на два символа справа
+        listBig.append(line_2[indexElement - countUpper : indexElement - 2])
+        countUpper = 0      # обнуляем для следующей проверки
+# добавлено дублирование на случай, если кол-во индексов строки в цикле закончится, но не хватит итерации для проверки (если в конце исходной строки к примеру стоит три большие буквы
+if countUpper > 2:
+    listBig.append(line_2[indexElement - countUpper: indexElement - 2])
+
+    '''
+    моя наверное пятая переделка так и не удавшегося кода (((
+for simbol in line_2:
+    if simbol.islower():
+        if len(value) > 2:
+            listBig.append(value[:-2])
+            countLower = 0
+            value = ''
+        elif line_2[(line_2.index(simbol) - 1)].isupper():
+            countLower = 0
+            value = ''
+        else:
+            value = ''
+
+        countLower += 1
+    elif simbol.isupper():
+        if countLower < 2:
+            countLower = 0
+            value = ''
+        else:
+            value += simbol
+'''
+
+print(listBig, 'длина списка без использования регулярки - ', len(listBig))
+
+
 
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
