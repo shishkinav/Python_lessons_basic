4deb05a2330e4835dc530ea482e7feb81a685b66
AnatolyShishkin <shishkinanatoli@gmail.com>
2018-10-11 23:03:09 +0300

    Урок 5 - Домашнее задание - Уровень Easy - Задача 1 - добавил код

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -5,6 +5,46 @@ __author__ = 'Шишкин Анатолий'
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+
+def addDir(nameDir):
+    '''
+    функция создаёт в текущей директории папку с передаваемым именем
+    '''
+    dirPath = os.path.join(os.getcwd(), nameDir)
+    try:
+        os.mkdir(dirPath)
+        print('директория {} создана'.format(nameDir))
+    except FileExistsError:
+        print('директория {} существует' .format(nameDir))
+
+def delDir(nameDir):
+    '''
+    функция удаляет из текущей директории папку с передаваемым именем
+    '''
+    dirPath = os.path.join(os.getcwd(), nameDir)
+    try:
+        os.rmdir(dirPath)
+        print('директория {} удалена'.format(nameDir))
+    except FileExistsError:
+        print('директории {} не существует' .format(nameDir))
+
+# весь этот блок ниже не будет доступен при импорте этого файла в качестве модуля
+if __name__ == "__main__":
+    while True:
+        change = input('[1] создать директории\n[2] удалить директории\n'
+                       '[3] выйти из программы\nВыберите номер действия:\n')
+        if change == '1':   # команда создания
+            for i in range(9):
+                addDir('dir_' + str(i + 1))
+        elif change == '2': # команда удаления
+            for i in range(9):
+                delDir('dir_' + str(i + 1))
+        elif change == '3': # команда выхода
+            break
+        else:               # некорректный номер ввода
+            print('номер действия введен неправильно')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
 
