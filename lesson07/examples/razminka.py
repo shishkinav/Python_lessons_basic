b93b598e25d4f42eba70692d4cd05ac179c452d8
AnatolyShishkin <shishkinanatoli@gmail.com>
2018-10-22 09:17:35 +0300

    Урок 7 - для анализа примера 'razminka.py' убирал комментирование, чтобы разобраться как будет работать

--- lesson07/examples/razminka.py
+++ lesson07/examples/razminka.py
@@ -1,36 +1,36 @@
 # coding: utf-8
 
-# class BaseClass():
-    # x = 1
-    # y = 2
+class BaseClass():
+    x = 1
+    y = 2
 
     
-# class ChildClass(BaseClass):
-    # x = 111
-    # y = 222
+class ChildClass(BaseClass):
+    x = 111
+    y = 222
     
-    # def mix(BaseClass):
-        # return BaseClass.y
+    def mix(BaseClass):
+        return BaseClass.y
         
         
-# c = ChildClass()
-# print(c.mix())        
+c = ChildClass()
+print(c.mix())
 
-# x = 13
-# def func():
-    # x = 1
-    # class c:
-        # x = 2
-        # print(x)
-        # y = input('y = ')
+x = 13
+def func():
+    x = 1
+    class c:
+        x = 2
+        print(x)
+        y = input('y = ')
         
-        # def m(self):
-            # print(x)
-    # i = c()
-    # i.m()        
+        def m(self):
+            print(x)
+    i = c()
+    i.m()
         
-# print(x)
-# func()
+print(x)
+func()
 
         
 
